21f15b228d77ecb1e3d1818141b61171009feb79
asvo <asvo@ucsd.edu>
2025-05-09 18:52:16 -0700

    Finish Expose

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,13 @@ function getRecipesFromStorage() {
 	// A9. TODO - Complete the functionality as described in this function
 	//           header. It is possible in only a single line, but should
 	//           be no more than a few lines.
+	const storage = JSON.parse(localStorage.getItem('recipes'));
+
+	if (storage === null) {
+		return [];
+	} else {
+		return storage;
+	}
 }
 
 /**
@@ -35,10 +42,17 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
 	// A10. TODO - Get a reference to the <main> element
+	const mainElement = document.querySelector('main'); 
+
 	// A11. TODO - Loop through each of the recipes in the passed in array,
 	//            create a <recipe-card> element for each one, and populate
 	//            each <recipe-card> with that recipe data using element.data = ...
 	//            Append each element to <main>
+	recipes.forEach((curr) => {
+		const toAdd = document.createElement('recipe-card');
+		toAdd.data = curr;
+		mainElement.appendChild(toAdd);
+	});
 }
 
 /**
